e51d2d41597d6692e20ecd52a5dbffe9bfe5d8a6
Jinny Shin <sjinny0124@gmail.com>
2019-01-28 17:05:54 +0900

    리액트 컴포넌트 예제

--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const myElem  = <p>이렇게 써도 되나</p>;
+
+const MyCom = () => <p>이렇게 써도 되나?</p>;
+
+class MyKlassCom extends React.Component {
+  render() {
+    return (
+    <div>
+      <p>클래스형 컴포넌트</p>
+      <MyCom />
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(<MyKlassCom />, document.getElementById('root'));
+
+console.log(MyCom);
+console.log(MyKlassCom);
+console.log(<MyKlassCom />);
+console.log(myElem, MyCom(), <myCom />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
