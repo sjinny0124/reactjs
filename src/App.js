5af811020d2accccbe3ffaf2c31d19cb6dc59304
Jinny Shin <sjinny0124@gmail.com>
2019-01-29 17:56:05 +0900

    Todos 컴포넌트

--- src/App.js
+++ src/App.js
@@ -4,35 +4,28 @@ import './App.css';
 
 import Todos from './components/Todos';
 
-const data = [
-  {name: 'React 개발에 필요한 환경을 구축한다.', completed: true},
-  {name: '새로운 자바스크립트 문법을 익힌다.', completed: true},
-  {name: '개발 편의를 위한 VSCode IDE를 익힌다.', completed: false},
-  {name: '기본적인 Git 사용법을 익힌다.', completed: true},
-  {name: 'PR 코드 리뷰를 응요한 개발 프로세스를 익힌다.', completed: true},
-  {name: 'React 로 간단한 노트 앱을 만들어 본다.', completed: true},
-];
-
 class App extends Component {
   state = {
-    data: data,
+    data: [
+      {name: 'React 개발에 필요한 환경을 구축한다.', completed: true},
+      {name: '새로운 자바스크립트 문법을 익힌다.', completed: true},
+      {name: '개발 편의를 위한 VSCode IDE를 익힌다.', completed: false},
+      {name: '기본적인 Git 사용법을 익힌다.', completed: true},
+      {name: 'PR 코드 리뷰를 응요한 개발 프로세스를 익힌다.', completed: true},
+      {name: 'React 로 간단한 노트 앱을 만들어 본다.', completed: true},
+    ],
     isUnmount: false,
   };
 
   componentDidMount() {
     setTimeout(() => {
+      const items = this.state.data;
+      const data = items.map(item => {
+        item.completed = true;
+        return item;
+      });
       this.setState({
-        data: [
-          {name: 'React 개발에 필요한 환경을 구축한다.', completed: true},
-          {name: '새로운 자바스크립트 문법을 익힌다.', completed: true},
-          {name: '개발 편의를 위한 VSCode IDE를 익힌다.', completed: false},
-          {name: '기본적인 Git 사용법을 익힌다.', completed: true},
-          {
-            name: 'PR 코드 리뷰를 응요한 개발 프로세스를 익힌다.',
-            completed: true,
-          },
-          {name: 'React 로 간단한 노트 앱을 만들어 본다.', completed: true},
-        ],
+        data: data,
       });
     }, 5000);
 
